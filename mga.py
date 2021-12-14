1a0e86b37d5fcc339f98b9c375e037d8f4e03f03
W. Milczarek <wmilczarek@gmail.com>
2021-12-14 20:27:25 +0100

    Created basic design for the app.
    The letter learning screen (main screen) now contains one bigger
    MDCard with random Georgian letter and four MDCars, which will be
    filled with 3 random letters of the user's alphabet and one
    correct answer.
    
    Below random Georgian letter is a temporary button, which picks
    a new letter after being clicked. It'll be removed when
    functionality will be added to 4 bottom letters.
    
    App design is based on MDBottomNavigation, which allows keeping
    multiple screens in one bottom navbar. Right now each screen has
    a temporary icon.

--- mga.py
+++ mga.py
@@ -1,30 +1,55 @@
+'''
+To do list:
+            *App design based on MDBottomNavigation
+            *Main screen with letter learning mode
+            *Time attack mode(?)
+            *Screen with basic words learning.
+            *Screen with history of Georgian writing.
+            *Settings screen.
+            *App strings in three languages: English, Polish and Russian
+            *Achievements(?)
+'''
+
+
 from kivymd.app import MDApp
 from kivy.lang.builder import Builder
 from kivy.uix.screenmanager  import Screen,ScreenManager
+from kivy.uix.gridlayout import GridLayout
+from kivy.core.window import Window
 import data_loader as dl
+import random
 
 load_strings = dl.load_lang_data()
 load_settings = dl.load_settings()
+georgian_letters = ['ა','ბ','გ','დ','ე','ვ','ზ','თ','ი','კ','ლ','მ','ნ','ო',
+'პ','ჟ','რ','ს','ტ','უ','ფ','ქ','ღ','ყ','შ','ჩ','ც','ძ','წ','ჭ','ხ','ჯ','ჰ']
+
+#Resolution which simulates mobile phone
+Window.size = (405,900)
 
 class SManager(ScreenManager):
     pass
 class MainMenu(Screen):
     pass
+class TopPanel(GridLayout):
+    pass
 
 
+#Main app
 class Mkhedruli(MDApp):
-
-
     def build(self):    
         self.settings = load_settings
         self.language_strings = load_strings
         app_uix = Builder.load_file('mga.kv')
         return app_uix
 
+    #Function for changing language settings in the app
     def change_language(self, lang):
         self.settings['language'] = lang[-2:]
-        self.root.get_screen('MainMenu').ids.apptitle.text = ''
         self.root.get_screen('MainMenu').ids.apptitle.text = self.language_strings['app_name'][self.settings['language']]
-        print(self.root.get_screen('MainMenu').ids.apptitle.text)
+
+    #Function for picking random Georgian letter in letter learning mode
+    def pick_georgian_letter(self):
+        self.root.get_screen('MainMenu').ids.geo_letter.text = random.choice(georgian_letters)
 
 Mkhedruli().run()
