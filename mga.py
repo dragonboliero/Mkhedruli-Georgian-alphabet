868a31e43b80b02be05d7ab6a54e1944564fb769
W. Milczarek <wmilczarek@gmail.com>
2021-12-15 21:44:51 +0100

    Mapped Georgian alphabet with en,pl and ru & made all fields random in the beginning.
    List of Georgian letters was replaced by dictionary with
    corresponding transcriptions in en,pl and ru.
    
    Now, after the app loads, a Geogrian letter is randomized
    and assigned to appropriate MDCard with it's corresponding
    lerner's language equivalent and three other random letters in
    this language.

--- mga.py
+++ mga.py
@@ -21,8 +21,26 @@ import random
 
 load_strings = dl.load_lang_data()
 load_settings = dl.load_settings()
-georgian_letters = ['ა','ბ','გ','დ','ე','ვ','ზ','თ','ი','კ','ლ','მ','ნ','ო',
-'პ','ჟ','რ','ს','ტ','უ','ფ','ქ','ღ','ყ','შ','ჩ','ც','ძ','წ','ჭ','ხ','ჯ','ჰ']
+
+georgian_letters_dict = {
+'ა':{'en':'a','pl':'a','ru':'а'},'ბ':{'en':'b','pl':'b','ru':'б'},
+'გ':{'en':'g','pl':'g','ru':'г'},'დ':{'en':'d','pl':'d','ru':'д'},
+'ე':{'en':'e','pl':'e','ru':'э'},'ვ':{'en':'v','pl':'w','ru':'в'},
+'ზ':{'en':'z','pl':'z','ru':'з'},'თ':{'en':'th','pl':'th','ru':'тх'},
+'ი':{'en':'i','pl':'i','ru':'и'},'კ':{'en':'k\'','pl':'k\'','ru':'к\''},
+'ლ':{'en':'l','pl':'l','ru':'л'},'მ':{'en':'m','pl':'m','ru':'м'},
+'ნ':{'en':'n','pl':'n','ru':'н'},'ო':{'en':'o','pl':'o','ru':'о'},
+'პ':{'en':'p\'','pl':'p\'','ru':'п\''},'ჟ':{'en':'zh','pl':'ż','ru':'ж'},
+'რ':{'en':'r','pl':'r','ru':'р'},'ს':{'en':'s','pl':'s','ru':'с'},
+'ტ':{'en':'t\'','pl':'t\'','ru':'т\''},'უ':{'en':'u','pl':'u','ru':'у'},
+'ფ':{'en':'ph','pl':'ph','ru':'пх'},'ქ':{'en':'kh','pl':'kh','ru':'кх'},
+'ღ':{'en':'gh','pl':'gh','ru':'гх'},'ყ':{'en':'q\'','pl':'q\'','ru':'к\''},
+'შ':{'en':'sh','pl':'sz','ru':'ш'},'ჩ':{'en':'tshh','pl':'czh','ru':'чх'},
+'ც':{'en':'tsh','pl':'tsh','ru':'цх'},'ძ':{'en':'dz','pl':'dz','ru':'дз'},
+'წ':{'en':'ts\'','pl':'ts\'','ru':'ц\''},'ჭ':{'en':'tsh\'','pl':'cz\'','ru':'ч\''},
+'ხ':{'en':'kh','pl':'kch','ru':'кх'},'ჯ':{'en':'j','pl':'dż','ru':'дж'},
+'ჰ':{'en':'h','pl':'h','ru':'х'}
+}
 
 #Resolution which simulates mobile phone
 Window.size = (405,900)
@@ -40,6 +58,10 @@ class Mkhedruli(MDApp):
     def build(self):    
         self.settings = load_settings
         self.language_strings = load_strings
+        self.first_geo_letter = random.choice(list(georgian_letters_dict))
+        self.first_correct_answer = georgian_letters_dict[self.first_geo_letter][self.settings['language']]
+        self.first_random_letters = [random.choice(list(georgian_letters_dict.keys())) for letter in range(3)]
+        self.first_random_letters = [georgian_letters_dict[self.first_random_letters[current_choice]][self.settings['language']] for current_choice in range(3)]
         app_uix = Builder.load_file('mga.kv')
         return app_uix
 
@@ -50,6 +72,6 @@ class Mkhedruli(MDApp):
 
     #Function for picking random Georgian letter in letter learning mode
     def pick_georgian_letter(self):
-        self.root.get_screen('MainMenu').ids.geo_letter.text = random.choice(georgian_letters)
+        self.root.get_screen('MainMenu').ids.geo_letter.text = random.choice(list(georgian_letters_dict))
 
 Mkhedruli().run()
